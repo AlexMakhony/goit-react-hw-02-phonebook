a6d915eaf6deb99a36deb7844fdab54ad32148c9
AlexMakhony <makhovskiy.sasha@gmail.com>
2023-03-22 12:30:45 +0200

    HW2-2-Шаг3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,18 @@ import {ContactList} from './ContactList/ContactList';
 export class App extends Component {
   constructor() {
     super();
+    // ШАГ 3 Змінюємо початковий стейт згідно ТЗ
     this.state = {
-      name: '',
-      contacts: [],
-    //   Додаємо ключ номеру
-      number: '',
-    };
+        name: '',
+        contacts: [
+        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
+        filter: '',
+        number: '',
+        };
   }
 
 //  ШАГ 1: Метод зміни значення у формі який буде рендеритись у імпуті
@@ -34,11 +40,21 @@ export class App extends Component {
     }));
   };
 
+//   ШАГ 3: Додаємо метод фільтру
+  handleFilterChange = (e) => {
+    this.setState({ filter: e.target.value });
+    };
+
   
 //  ШАГ1: Рендеримо цю історію
 //  ЩАШ2: перероблюємо наш рендер, додавши інпут №телефону згідно ТЗ
 render() {
-    const { contacts, name, number } = this.state;
+    // ШАГ3: Додаємо ключ Фільтр
+    const { contacts, name, number, filter } = this.state;
+    // ШАГ3: Додаємо відфільтровані контакти, які приводимо до ловеркейсу
+    const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
     return (
       <div>
         <h1>Phonebook</h1>
@@ -70,7 +86,10 @@ render() {
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
-        <ContactList contacts={contacts} />
+        {/* ШАГ3: Додаємо імпут для пошуку контакту по Name */}
+        <input type="text" value={filter} onChange={this.handleFilterChange} placeholder="Search by name" />
+        {/* Шаг3: Тепер пережаємо пром не контактс, а відвільтровані контакти */}
+        <ContactList contacts={filteredContacts} />
       </div>
     );
   }
